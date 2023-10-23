1ba730020ffe763003e34e679b9674275ef0215e
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-10-23 22:15:41 +0000

    Deploying to gh-pages from @ dev0652/zenbit-test@1dc9c1e65821a505b76498d47f0b54e68c225de0 🚀

---
+++ static/js/637.f69051c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkzenbit_test=self.webpackChunkzenbit_test||[]).push([[637],{3637:function(e,t,n){n.r(t);var s=n(6907),i=n(8282),c=n(3329);t.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsxs)(s.ql,{children:[(0,c.jsx)("title",{children:"Login"}),(0,c.jsx)("meta",{name:"description",content:"Log in to be able to accept deals"})]}),(0,c.jsx)(i.CS,{children:(0,c.jsx)(i.kW,{formType:"login"})})]})}}}]);
+//# sourceMappingURL=637.f69051c3.chunk.js.map
